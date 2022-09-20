46674523149759533d228229d023fba7b7fa4f5f
Angela Felix <angela@isolveproduce.com>
2022-09-20 14:01:43 -0600

    Second Exercisee

--- types-dates-objects-exercise.js
+++ types-dates-objects-exercise.js
@@ -1,11 +1,13 @@
-const name = "Angela";
-const age = 23;
-const developer = true;
-const birthday = new Date(1998,10,18);
-const favoriteBook = {
+const lista = [
+    "Angela",
+    23,
+    true,
+    new Date(1998,10,18),
+    {
     title: "Temporada de Huracanes",
     autor: "Fernanda Melchor",
     date: new Date(2017,4,12),
     url: "https://www.amazon.com.mx/Temporada-huracanes-Fernanda-Melchor/dp/6073152736"
-};
+    },
+];
 
